59a8738a5fbd136d12aed4df7c79f38318450b43
Sahil Choudhary <sahil.codes.it@gmail.com>
2019-08-23 00:00:01 +0530

    minor bug fix

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
+
 # Create your views here.
 def home(request):
 	return render(request, 'products/home.html')
@@ -14,12 +15,12 @@ def create(request):
 			product.title = request.POST['title']
 			product.body = request.POST['body']
 			if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
-				product.url = request.POST['title']
+				product.url = request.POST['url']
 			else:
-				product.url = 'http://' + request.POST['title']
+				product.url = 'http://' + request.POST['url']
 				
 			product.icon = request.FILES['icon']
-			product.title = request.FILES['image']
+			product.image = request.FILES['image']
 			product.pub_date = timezone.datetime.now()
 			product.hunter = request.user
 			product.save()
@@ -28,3 +29,8 @@ def create(request):
 			return render(request, 'products/create.html', {'error':'All fields are required'}) 
 	else:
 		return render(request, 'products/create.html')
+
+
+def detail(request, product_id):
+	product = get_object_or_404(Product, pk=product_id)
+	return render(request, 'products/detail.html', {'product':product})
